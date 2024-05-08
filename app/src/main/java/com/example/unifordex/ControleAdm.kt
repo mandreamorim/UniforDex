de31d589708b4e3281c1005807bc17d49c0a40cd
Marcos Amorim <mandreamorim@gmail.com>
2024-05-08 20:17:59 -0300

    parte do thiago
    editar a partir daqui

--- app/src/main/java/com/example/unifordex/ControleAdm.kt
+++ app/src/main/java/com/example/unifordex/ControleAdm.kt
@@ -1,20 +1,33 @@
 package com.example.unifordex
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ControleAdm : AppCompatActivity() {
+
+    private lateinit var btnAdd: Button
+    private lateinit var btnLogOut: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_controle_adm)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnAdd = findViewById(R.id.btn_ir_para_criar_expo)
+        btnLogOut = findViewById(R.id.button_volta_para_adm)
+
+        btnAdd.setOnClickListener {
+            val proxima = Intent(this,CriarExposicao::class.java)
+            startActivity(proxima)
+        }
+        btnLogOut.setOnClickListener {
+            val proxima2 = Intent(this,LoginAdm::class.java)
+            startActivity(proxima2)
+            finish()
         }
     }
 }
